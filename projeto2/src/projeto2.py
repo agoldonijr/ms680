44681c95e36fbe1623a28e2d3363e4cdc3444f89
Alcides Goldoni Junior <goldoni@ggaunicamp.com>
2016-11-24 23:20:43 -0200

    ordem de grandeza acertadas. Verificar para periodos grandes o pq dos predadores continuarem crescendo

--- projeto2/src/projeto2.py
+++ projeto2/src/projeto2.py
@@ -4,63 +4,74 @@
 import matplotlib.pyplot as plt
 
 #Inicializacao das variaveis
-p = []			#Vetor que contará a quantidade de poluicao
-a = []			#Vetor que conterá os indiviuos da especie A
-b = []			#Vetor que conterá os indiviuos da especie B
-c = []			#Vetor que conterá os indiviuos da especie C
-d = []			#Vetor que conterá os indiviuos da especie D
+p = []				#Vetor que contará a quantidade de poluicao
+a = []				#Vetor que conterá os indiviuos da especie A
+b = []				#Vetor que conterá os indiviuos da especie B
+c = []				#Vetor que conterá os indiviuos da especie C
+d = []				#Vetor que conterá os indiviuos da especie D
 
-p.append(0);		#inicializando o vetor de poluicao
-a.append(1000);		#inicializando o vetor da especie A
-b.append(2000);		#inicializando o vetor da especie B
-c.append(3000);		#inicializando o vetor da especie C
-d.append(100);		#inicializando o vetor da especie D
+p.append(0);			#inicializando o vetor de poluicao
+a.append(10000);		#inicializando o vetor da especie A
+b.append(20000);		#inicializando o vetor da especie B
+c.append(30000);		#inicializando o vetor da especie C
+d.append(100);			#inicializando o vetor da especie D
 
-lambdaA = 0.025		#taxa de crescimento da especie A
-lambdaB = 0.030		#taxa de crescimento da especie B
-lambdaC = 0.035		#taxa de crescimento da especie C
-lambdaD = 0.005		#taxa de crescimento da especie D
+lambdaA = 0.05			#taxa de crescimento da especie A
+lambdaB = 0.060			#taxa de crescimento da especie B
+lambdaC = 0.06			#taxa de crescimento da especie C
+lambdaD = 0.008			#taxa de crescimento da especie D
 
-alpha1 	= 0.0005		#taxa de decrescimento por predacao da especie A
-alpha2 	= 0.0015		#taxa de decrescimento por poluicao da especie A 
-beta1  	= 0.0008		#taxa de decrescimento por predacao da especie B 
-beta2	= 0.0015		#taxa de decrescimento por poluicao da especie B 
-gama1	= 0.0009		#taxa de decrescimento por predacao da especie C 
-gama2	= 0.0015		#taxa de decrescimento por poluicao da especie C 
+alpha1 	= 0.00000005		#taxa de decrescimento por predacao da especie A
+alpha2 	= 0.0050		#taxa de decrescimento por poluicao da especie A 
+beta1  	= 0.00000008		#taxa de decrescimento por predacao da especie B 
+beta2	= 0.0056		#taxa de decrescimento por poluicao da especie B 
+gama1	= 0.00000009		#taxa de decrescimento por predacao da especie C 
+gama2	= 0.0058		#taxa de decrescimento por poluicao da especie C 
 
-epsilon = 0.0012	#taxa de decaimento do poluente
+y 	= 0.0000000025		#taxa de crescimento do predador enquanto tem presa
 
-K	= 10000		#Capacidade de suporte do meio
 
-n	= 106		#tempo de simulacao em semanas (aproximadamente 53 semanas = 1 ano)
-x	= 20		#semana em que ocorre o acidente
+epsilon = 0.05			#taxa de decaimento do poluente
 
-#print ("a ", a[0], "b ", b[0], "c ", c[0], "d ",d[0])
+K	= 100000		#Capacidade de suporte do meio
+
+n	= 106			#tempo de simulacao em semanas (aproximadamente 53 semanas = 1 ano)
+x	= 20			#semana em que ocorre o acidente
 
 #Laco de tempo e a situacao de cada um dos individuos da dinamica e no instante n ocorre o acidente
 for i in range(0,n):
+	#ocorrecia do acidente
 	if i == x:
-		a.append(a[i] - (lambdaA *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) -alpha1*a[i]*d[i]))
-		b.append(b[i] - (lambdaB *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) -beta1*b[i]*d[i]))
-		c.append(c[i] - (lambdaC *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) -gama1*c[i]*d[i]))
-		d.append(d[i] - (lambdaC *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) + alpha1*a[i]*d[i] +beta1*b[i]*d[i]+ gama1*c[i]*d[i]))
-		p.append(100.00);
+		a.append(a[i] + (lambdaA *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) - alpha1*a[i]*d[i]))
+		b.append(b[i] + (lambdaB *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) - beta1*b[i]*d[i]))
+		c.append(c[i] + (lambdaC *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) - gama1*c[i]*d[i]))
+		d.append(d[i] + (lambdaC *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) + y*a[i]*d[i] +y*b[i]*d[i]+ y*c[i]*d[i]))
+		p.append(100);
+		print ("Poluente entrou")
+	
+	#a partir daqui o acidente aconteceu temos poluicao na agua
 	elif i > x:
-		a.append(a[i] - (lambdaA *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) -alpha1*a[i]*d[i]))
-		b.append(b[i] - (lambdaB *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) -beta1*b[i]*d[i]))
-		c.append(c[i] - (lambdaC *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) -gama1*c[i]*d[i]))
-		d.append(d[i] - (lambdaC *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) + alpha1*a[i]*d[i] +beta1*b[i]*d[i]+ gama1*c[i]*d[i]))
+		a.append(a[i] + (lambdaA *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) - alpha1*a[i]*d[i] - alpha2*p[i]*a[i]))
+		b.append(b[i] + (lambdaB *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) - beta1*b[i]*d[i] - beta2*p[i]*b[i]))
+		c.append(c[i] + (lambdaC *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) - gama1*c[i]*d[i] - gama2*p[i]*c[i]))
+		d.append(d[i] + (lambdaC *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) + y*a[i]*d[i] +y*b[i]*d[i]+ y*c[i]*d[i]))
 		p.append(p[i] - p[i]*epsilon);
-	else:
-		a.append(a[i] - (lambdaA *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) -alpha1*a[i]*d[i] - alpha2*p[i]*a[i]))
-		b.append(b[i] - (lambdaB *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) -beta1*b[i]*d[i] - beta2*p[i]*b[i]))
-		c.append(c[i] - (lambdaC *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) -gama1*c[i]*d[i] - gama2*p[i]*c[i]))
-		d.append(d[i] - (lambdaC *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) + alpha1*a[i]*d[i] +beta1*b[i]*d[i]+ gama1*c[i]*d[i]))
+	
+	#o acidente ainda nao occoreu
+	else:	
+		a.append(a[i] + (lambdaA *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) - alpha1*a[i]*d[i]))
+		b.append(b[i] + (lambdaB *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) - beta1*b[i]*d[i]))
+		c.append(c[i] + (lambdaC *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) - gama1*c[i]*d[i]))
+		d.append(d[i] + (lambdaC *(a[i] + b[i] + c[i] +d[i])*( 1 - (a[i] + b[i] + c[i] +d[i])/K) + y*a[i]*d[i] + y*b[i]*d[i]+ y*c[i]*d[i]))
 		p.append(p[i] + 0);
-	print(a[i])
+	print(i, a[i])
 
 #Plotando o grafico
 plt.plot(a, 'bo-');
+plt.plot(b, 'go-');
+plt.plot(c, 'yo-');
+plt.plot(d, 'ro-');
+#plt.plot(p, 'ko-');
 plt.ylabel('Poluicao');
 plt.xlabel('Semana');
 plt.show();
